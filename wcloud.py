d704ee0924a2276d855458a932ade6a8d1b2fe5c
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2020-10-14 15:49:04 +0300

    add counter_speech wordcloud

--- wcloud.py
+++ wcloud.py
@@ -1,5 +1,7 @@
 import json
 import nltk
+import matplotlib
+
 from nltk.corpus import stopwords
 
 from wordcloud import WordCloud
@@ -17,24 +19,45 @@ json_data = f.read()
 dataset = json.loads(json_data)
 
 # form a dict with the word as a key and its frequency as the value
-wordcount = {}
+hatespeech_count = {}
+counterspeech_count = {}
 for tweet in dataset["conan"]:
     hate_speech = tweet["hateSpeech"]
-    words = nltk.word_tokenize(hate_speech)
-    words = [word for word in words if word not in en_stops]
-    for word in words:
-        wordcount.setdefault(word, 0)
-        wordcount[word] += 1
+    counter_speech = tweet["counterSpeech"]
+    hatewords = nltk.word_tokenize(hate_speech)
+    hatewords = [word for word in hatewords if word not in en_stops]
+    counterwords = nltk.word_tokenize(counter_speech)
+    counterwords = [word for word in counterwords if word not in en_stops]
+    # go through both hate speech and counter speech
+    for word in hatewords:
+        hatespeech_count.setdefault(word, 0)
+        hatespeech_count[word] += 1
+    for word in counterwords:
+        counterspeech_count.setdefault(word, 0)
+        counterspeech_count[word] += 1
 
 # wordcloud takes a string as input, so go over the dict and concatenate a string
-texts = ""
-counts = []
-for key, value in wordcount.items():
-    texts = texts + key + ' '
-    counts.append(value)
+hate_texts = ""
+hate_counts = []
+for key, value in hatespeech_count.items():
+    hate_texts = hate_texts + key + ' '
+    hate_counts.append(value)
+
+counter_texts = ""
+counter_counts = []
+for key, value in counterspeech_count.items():
+    counter_texts = counter_texts + key + ' '
+    counter_counts.append(value)
 
 # form the wordcloud and print it out
-wordcloud = WordCloud().generate(texts)
+wordcloud = WordCloud().generate(hate_texts)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-plt.show()
+plt.savefig("hatespeech.png")
+# plt.show()
+
+counter_wordcloud = WordCloud().generate(counter_texts)
+plt.imshow(counter_wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.savefig("counterspeech.png")
+# plt.show()

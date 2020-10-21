0d2296d92801289975a8232233166a1a857e92bc
Jauhiainen Atte <atte.jauhiainen@uros.com>
2020-10-21 16:44:09 +0300

    Add another NN layer, improve logs, save the model to disk

--- heuristic.py
+++ heuristic.py
@@ -63,26 +63,30 @@ def teach(hate_tweets, counter_tweets):
         # counterspeech is label 0
         train_labels.append(0)
 
-    # print(train_items[0])
-    # print(train_labels[0])
 
     model = tf.keras.Sequential([
         # each train_items item has 197 fields, so that is the number of neurons we need in the first layer of network
         tf.keras.layers.Dense(197, activation='relu'),
-        tf.keras.layers.Dense(10)
+        tf.keras.layers.Dense(25), # Not sure what to do with this layer. Do we even need it
+        tf.keras.layers.Dense(2)
     ])
+
+
+    # Optimizer uses adaptive learning rates (adam optimizer)
     model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
     print("Starting to teach")
     model.fit(train_items, train_labels, epochs=10)
+    model.summary()
     return model
+
 
 def evaluate(model, hate_tweets, counter_tweets):
     evaluate_items = []
     evaluate_labels = []
-    print("Parsing evaluate data")
+    print("Parsing evaluation data")
     for tweet in hate_tweets:
         categories = list(get_categories(tweet).values())
         scores = list(get_inquiries(tweet).values())
@@ -102,7 +106,7 @@ def evaluate(model, hate_tweets, counter_tweets):
     print("Evaluating the model")
     test_loss, test_acc = model.evaluate(evaluate_items, evaluate_labels, verbose=2)
 
-    print('\nTest accuracy:', test_acc)
+    print("Accuracy of the heuristic against CONAN dataset: ", test_acc)
 
 if __name__ == '__main__':
     import sys
@@ -119,3 +123,6 @@ if __name__ == '__main__':
     # Use the first part of the dataset to teach the network, and second part to evaluate it
     model = teach(hate_words[:data_split], counter_words[:data_split])
     evaluate(model, hate_words[data_split:], counter_words[data_split:])
+
+    # Save the model to disk
+    model.save('./heuristic')
